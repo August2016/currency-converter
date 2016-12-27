d96be394f874f616bff444549b073153dbffb0d0
tiagoschmitt <tiago.schmitt@gmail.com>
2016-12-27 22:53:25 +0400

    Update CurrencyService.js

--- app/currency/CurrencyService.js
+++ app/currency/CurrencyService.js
@@ -7,15 +7,15 @@ export class CurrencyService {
 
   //call the endpoint asynchronously - 'value': value to convert, 'from': base currency to convert, 'to': the final currency rate
   async convert(value, from, to) {
-  	try {
-  	  let promise = await fetch(`http://api.fixer.io/latest?base=${from}&symbols=${to}`);
+    try {
+      let promise = await fetch(`http://api.fixer.io/latest?base=${from}&symbols=${to}`);
       let result = await promise.json();
-  	  let rate = parseFloat(result.rates[to]);
+      let rate = parseFloat(result.rates[to]);
 
-  	  return rate * value;
-  	}
-	  catch (e) {
+      return rate * value;
+    }
+    catch (e) {
       return -1;
-	  }
+    }
   }
 }
